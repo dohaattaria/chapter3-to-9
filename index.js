02b17bcbd2d38287422c013800438e8a4d3c7f18
dohaattaria <dohafatima484@gmail.com>
2024-10-14 23:10:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,81 @@
+// chapter 01
+// alert
+
+alert("WELL COME TO MY WEBSITE!" );
+
+alert("HELLO WORLD");
+
+// chapter 02
+//   declares a variable
+
+var tittle = "Hello, world!";
+alert(tittle);
+
+// Declare a variable age
+var age;
+age = 18;
+alert(age);
+
+
+// Declare a variable greeting
+var greeting = "Good morning!";
+alert(greeting);
+
+
+greeting = "Good night!";
+alert(greeting);
+
+
+// Declare a variable called Color
+var Color = "pink";
+alert("My favorite color is " + Color + ".");
+
+// chapter 03
+// Declare and assign 170 to height
+
+var height = 170;         
+var newheight = height + 10; 
+
+alert(newheight);  
+
+// declare a number
+
+var num1 = 50;            
+var num2 = 30; 
+var sum = num1 + num2;    
+
+alert(sum);
+
+// / Declare numToBeAdded 
+
+let numToBeAdded = 20;  
+let total = numToBeAdded;  
+
+alert(total);
+
+// Declare a variable price with the value
+
+let price = 120;
+let totalPrice = price + (price * 0.10);
+alert("Total Price: $" + totalPrice);
+
+// chapter 04
+// replace with your city name
+
+let myAddress = "malir"; 
+alert("My Address: " + myAddress);
+
+// declare three variables using camelCase:
+
+let userName = "student ";
+let userEmail = "smit@gmail";
+let userPassword = "smit123";
+
+alert("User Name: " + userName + "\nUser Email: " + userEmail + "\nUser Password: " + userPassword);
+
+// chapter 05
+// Adding two numbers the result to the variable sum
+
+let sum = 25 + 30;
+alert("The sum is: " + sum);
+
